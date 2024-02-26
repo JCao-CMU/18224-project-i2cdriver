40ffcaf23f3ab00457f411de6fff359dad6c95d6
Jeffery Cao <75282529+JCao-CMU@users.noreply.github.com>
2024-02-25 20:48:46 -0500

    initial draft

--- i2c.sv
+++ i2c.sv
@@ -5,14 +5,16 @@ module I2C_slave (
     input logic clock, reset, 
 
     // interface with I2C
-    inout logic SDA, 
+    inout tri SDA, 
     input logic SCL, // We are fast enough, no need to do clock stretching
 
     // interface with downstream thread
-    inout logic [7:0] data, 
+    inout tri [7:0] data, 
     output logic writeOK, 
     input logic data_in
 );
+
+    parameter I2C_ADDRESS = 7'h49;
 
     // ==================================================================
     // ============= Four little FSM for Pattern Detection: =============
@@ -184,11 +186,199 @@ module I2C_slave (
 
     PISO the_piso (.clock, .reset, .data_in(data), .spit(piso_spit), 
                    .load(piso_load), .out(piso_out), .empty(piso_empty));
+
+    logic is_correct_address;
+
+    assign is_correct_address = sipo_out[7:1] == I2C_ADDRESS;
+
+    logic r1w0;
+
+    assign r1w0 = sipo_out[0];
+
+    logic ack, wr_up, wr_down;
+
+    assign SDA = ack ? 1'b0 : (wr_up ? piso_out : 1'bz);
+
+    assign data = wr_down ? register_out : 8'bz;
+
+    assign writeOK = ~wr_down & empty;
+
+    assign piso_load = ~wr_down & data_in & empty;
     
     // ====================================================
     // =============== The Main Control FSM ===============
     // ====================================================
-                   
+
+    enum logic [3:0] {IDLE, ADDR_RD, WAIT_END, 
+                      READ, READ_ACK, READ_PRELD, READ_LOAD, 
+                      WRITE, WRITE_ACK, WRITE_PRELD, WRITE_SEND, WRITE_RELEASE, 
+                      WRITE_CHECK_ACK} 
+                      state, nextstate;
+
+    always_comb begin : main_FSM
+        sipo_clear = 1'b0;
+        sipo_load = 1'b0;
+        ack = 1'b0;
+        store = 1'b0;
+        wr_down = 1'b0;
+        piso_spit = 1'b0;
+        wr_up = 1'b0;
+        unique case (state)
+            IDLE: begin
+                if (i2c_start) begin
+                    sipo_clear = 1'b1;
+                    nextstate = ADDR_RD;
+                end
+                else begin
+                    nextstate = IDLE;
+                end
+            end
+            ADDR_RD: begin
+                if (scl_rise && ~sipo_full) begin
+                    sipo_load = 1'b1;
+                end
+                else if (sipo_full && ~is_correct_address) begin 
+                    // we remove scl_rise here since when sipo is full and scl rise, 
+                    // we need to pull the ACK line. this will make us run out of 
+                    // time for checking address and RW
+                    nextstate = WAIT_END;
+                end
+                else if (sipo_full && is_correct_address && ~r1w0) begin
+                    // State READ is withrespect to slave, where i2c signal 
+                    // r1w0 is with respect to master. so need to flip this part. 
+                    nextstate = READ;
+                end
+                else if (sipo_full && is_correct_address && r1w0) begin
+                    nextstate = WRITE;
+                end
+                else begin
+                    nextstate = ADDR_RD;
+                end
+            end
+            WAIT_END: begin 
+                // This state is to made sure that this line don't 
+                // hijack the line in the middle of the i2c. 
+                if (i2c_stop)
+                    nextstate = IDLE;
+                else
+                    nextstate = WAIT_END;
+            end
+
+            /* READ SIDE */
+
+            READ: begin
+                if (scl_fall) begin
+                    ack = 1'b1;
+                    nextstate = READ_ACK;
+                end else begin
+                    nextstate = READ;
+                end
+            end
+            READ_ACK: begin
+                wr_down = 1'b1;
+                if (scl_fall) begin
+                    sipo_clear = 1'b1;
+                    nextstate = READ_PRELD;
+                end else begin
+                    ack = 1'b1;
+                    nextstate = READ_ACK;
+                end
+            end
+            READ_PRELD: begin
+                wr_down = 1'b1;
+                if (scl_rise) begin
+                    sipo_load = 1'b1;
+                    nextstate = READ_LOAD;
+                end else begin
+                    nextstate = READ_PRELD;
+                end
+            end
+            READ_LOAD: begin
+                wr_down = 1'b1;
+                if (scl_rise && ~sipo_full) begin
+                    sipo_load = 1'b1;
+                    nextstate = READ_LOAD;
+                end
+                else if (scl_fall && ~sipo_full) begin
+                    ack = 1'b1;
+                    store = 1'b1;
+                    nextstate = READ_ACK; 
+                    // end of communication, send acknowledgement
+                end
+                else if (sda_rise && scl_high) begin
+                    // sda change value during scl_low. if sda_rise and scl_high
+                    nextstate = IDLE;
+                end
+            end
+
+            /* WRITE SIDE */
+
+            WRITE: begin
+                if (scl_fall) begin
+                    ack = 1'b1;
+                    nextstate = WRITE_ACK;
+                end else begin
+                    nextstate = WRITE;
+                end
+            end
+            WRITE_ACK: begin
+                if (scl_fall) begin
+                    nextstate = WRITE_PRELD;
+                end else begin
+                    ack = 1'b1;
+                    nextstate = WRITE_ACK;
+                end
+            end
+            WRITE_PRELD: begin
+                if (scl_rise) begin
+                    piso_spit = 1'b1;
+                    wr_up = 1'b1;
+                    nextstate = WRITE_SEND;
+                end else begin
+                    nextstate = WRITE_PRELD;
+                end
+            end
+            WRITE_SEND: begin
+                if (scl_rise && ~piso_empty) begin
+                    piso_spit = 1'b1;
+                    wr_up = 1'b1;
+                    nextstate = WRITE_SEND;
+                end
+                else if (scl_fall && piso_empty) begin
+                    nextstate = WRITE_RELEASE;
+                end
+                else begin
+                    wr_up = 1'b1; // we need to hold this line. 
+                end
+            end
+            WRITE_RELEASE: begin // this state releases the SDA line
+                if (scl_rise) begin
+                    nextstate = WRITE_CHECK_ACK;
+                end
+                else begin
+                    nextstate = WRITE_RELEASE;
+                end
+            end
+            WRITE_CHECK_ACK: begin // This is a short check on master's ACK
+                if (sda_low) begin // ACKED
+                    nextstate = WRITE_PRELD;
+                end
+                else begin // NACKED
+                    nextstate = IDLE; // Communication end. no need to check STOP
+                end
+            end
+
+            default: nextstate = IDLE;
+        endcase
+    end
+
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset)
+            state = IDLE;
+        else
+            state <= nextstate;
+    end
+
 endmodule
 
 module SIPO #(
