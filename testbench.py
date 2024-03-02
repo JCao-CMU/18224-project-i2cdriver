e0e2b2c8396b74809cfec05683783fd412e3f8b3
Jeffery Cao <75282529+JCao-CMU@users.noreply.github.com>
2024-03-02 15:50:18 -0500

    idk

--- testbench.py
+++ testbench.py
@@ -31,81 +31,109 @@ async def send_bit(bit):
 async def send_data(address, array):
     # Address: int, array: data array of bits
     dut.SDA_in.value = 1
-    dut.SCL_in.value = 1
+    dut.SCL.value = 1
     await FallingEdge(dut.clock)
     dut.SDA_in.value = 0
     await FallingEdge(dut.clock)
-    dut.SCL_in.value = 0
+    dut.SCL.value = 0
     wt(2)
     for i in hex_to_binarray(address):
         send_bit(i)
     send_bit(0)
     #! RELEASE_WIRE, wait, and assert SDA_out is low and wr_up is high
     wt(2)
-    dut.SCL_in.value = 1
+    dut.SCL.value = 1
     wt(5)
-    dut.SCL_in.value = 0
-    
-    
-    # task send_bytes (input logic [6:0] address, input logic [7:0] payload []);
-    #     SDA_in = 1'b1;
-    #     SCL = 1'b1;
-    #     @(posedge clock)
-    #     SDA_in <= 1'b0;
-    #     @(posedge clock)
-    #     SCL <= 1'b0;
-    #     @(posedge clock);
-    #     @(posedge clock);
-    #     for (int i = 6; i >= 0; i--) begin
-    #         set_value(address[i]);
-    #     end
-    #     set_value(1'b0); // WRITE, so read from slave's perspective
-    #     SDA_in <= 1'bz;
-    #     wt(3);
-    #     assert (SDA_out == 1'b0) 
-    #         else $error("Failure to ACKNOWLEDGE: SDA=%b", SDA);
-    #     @(posedge clock);
-    #     @(posedge clock);
-    #     SCL <= 1'b1;
-    #     wait_5_clocks();
-    #     SCL <= 1'b0;
-    #     wait_5_clocks();
-    #     foreach (payload[k]) begin
-    #         for (int j = 7; j >= 0; j--) begin
-    #             set_value(payload[k][j]);
-    #         end
-    #         SDA <= 1'bz;
-    #         wt(3);
-    #         assert (SDA == 1'b0) 
-    #             else $error("Failure to ACKNOWLEDGE: SDA=%b", SDA);
-    #         @(posedge clock);
-    #         @(posedge clock);
-    #         SCL <= 1'b1;
-    #         wait_5_clocks();
-    #         SCL <= 1'b0;
-    #         wait_5_clocks();
-    #         assert (data == payload[k]) 
-    #             else $error("Received %h, Send %h", data, payload[k]);
-    #     end
-    #     SDA <= 1'b0;
-    #     SCL <= 1'b0;
-    #     @(posedge clock)
-    #     SCL <= 1'b1;
-    #     @(posedge clock)
-    #     SDA <= 1'b1;
-    # endtask
-    pass
+    dut.SCL.value = 0
+    for subarray in array:
+        for element in subarray:
+            send_bit(element)
+        #! RELEASE_WIRE, wait, and assert SDA_out is low and wr_up is high
+        wt(2)
+        dut.SCL.value = 1
+        wt(5)
+        dut.SCL.value = 0
+        wt(5)
+        payload = int("".join(str(x) for x in subarray), 2)
+        assert(dut.data_out.value == payload)
+    dut.SCL.value = 0
+    wt(1)
+    dut.SCL.value = 1
+    wt(1)
+    dut.SDA.value = 1
 
-def generate_packet():
+async def read_bit():
+    wt(3)
+    wt(2)
+    dut.SCL.value = 1
+    wt(2)
+    value = dut.SDA_out.value
+    wt(3)
+    dut.SCL.value = 0
+    return value
+
+async def recv_data(address, array):
+    # Address: int, array: array of bytes
+    dut.SDA_in.value = 1
+    dut.SCL.value = 1
+    await FallingEdge(dut.clock)
+    dut.SDA_in.value = 0
+    await FallingEdge(dut.clock)
+    dut.SCL.value = 0
+    wt(2)
+    for i in hex_to_binarray(address):
+        send_bit(i)
+    send_bit(0)
+    #! RELEASE_WIRE, wait, and assert SDA_out is low and wr_up is high
+
+    # todo: put thing in array.
+    for i in range(len(array)):
+        received_bit = []
+        wt(2)
+        dut.SCL.value = 1
+        dut.data_in.value = array[i]
+        wt(5)
+        dut.SCL.value = 0
+        for i in range(8):
+            bit = send_bit(element)
+            received_bit.append(bit)
+        #! DUT Releases wire, wait, and assert SDA_out is low and wr_up is high
+        if (i == len(array)-1):
+            dut.SDA_in.value = 1
+        else:
+            dut.SDA_in.value = 0
+        wt(2)
+        dut.SCL.value = 1
+        wt(5)
+        dut.SCL.value = 0
+        wt(2)
+        payload = int("".join(str(x) for x in received_bit), 2)
+        assert(array[i] == payload)
+    dut.SCL.value = 0
+    wt(1)
+    dut.SCL.value = 1
+    wt(1)
+    dut.SDA.value = 1
+
+def generate_down_packet():
     rand_packet_count = random.randint(PACKNUM_FLOOR, PACKNUM_CEIL+1)
     arr = []
     for i in range (rand_packet_count):
         packet = []
-        for i in range(8)
+        for i in range(8):
             bit = np.random.choice([0,1]) 
             packet.append(bit)
         arr.append(packet)
+    return arr
 
+def generate_up_packet():
+    rand_packet_count = random.randint(PACKNUM_FLOOR, PACKNUM_CEIL+1)
+    arr = []
+    for i in range (rand_packet_count):
+        packet = random.randint(0, 2**8)
+        arr.append(packet)
+    return arr
+    
 
 @cocotb.test()
 async def basic_test(dut):
@@ -125,21 +153,8 @@ async def basic_test(dut):
     await FallingEdge(dut.clock)
     dut.reset.value = False
 
-    dut.data_in.value = 32
-    dut.go.value = True
-    await FallingEdge(dut.clock)
-    dut.go.value = False
-
-    dut.data_in.value = 32
-    await FallingEdge(dut.clock)
-    dut.data_in.value = 13
-    await FallingEdge(dut.clock)
-    dut.data_in.value = 79
-    await FallingEdge(dut.clock)
-
-    dut.data_in.value = 40
-    dut.finish.value = 1
-    await FallingEdge(dut.clock)
-    print(dut.finish.value)
-    print(dut.range.value)
-    assert dut.range.value == 66
+    for i in range(20):
+        package_down = generate_down_packet()
+        send_data(0x49, package_down)
+        package_up = generate_up_packet()
+        recv_data(0x49, package_up)
